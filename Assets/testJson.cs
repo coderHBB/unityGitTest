9fce387377028ccf1ec59978bed8d48c3b57942f
coderHBB <developer@houseofbluebeans.com>
2015-08-20 18:22:59 +0530

    first commit

---
+++ Assets/testJson.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+using SimpleJSON;
+using System.IO;
+
+public class testJson : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+
+}
